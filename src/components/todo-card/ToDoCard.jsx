2c9a9723b5143df1d03e9bdf22cafdec44eae166
Oleksii Butrin <bootreen@gmail.com>
2024-06-25 15:57:49 +0200

    Finished ordered functionality

---
+++ src/components/todo-card/ToDoCard.jsx
@@ -0,0 +1,26 @@
+import "./ToDoCard.css";
+import { useToDoActions } from "../../store/store";
+
+export const ToDoCard = ({ id, title, desc, isFinished }) => {
+  const { onCheckClick, onCloseClick } = useToDoActions();
+  return (
+    <div className='task-card'>
+      <div className='header-container'>
+        <h3>{title}</h3>
+        <div className='control-container'>
+          <div className='control-button' onClick={() => onCheckClick(id)}>
+            {isFinished ? (
+              <span className='finished'>{"\u2714"}</span>
+            ) : (
+              <span className='unfinished'>{"\u26F6"}</span>
+            )}
+          </div>
+          <div className='control-button' onClick={() => onCloseClick(id)}>
+            <span className='close'>{"\u2716"}</span>
+          </div>
+        </div>
+      </div>
+      <p>{desc}</p>
+    </div>
+  );
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,59 @@
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 
-export const useStore = create(
+export const useToDoStore = create(
   immer((set, get) => ({
     toDoList: {},
 
+    newTask: {
+      isStarted: false,
+      title: "",
+      desc: "",
+    },
+
     actions: {
-      placeholder: () => set({ toDoList: get() }),
+      onCloseClick: (id) =>
+        set((state) => {
+          delete state.toDoList[id];
+          return state;
+        }),
+
+      onCheckClick: (id) =>
+        set((state) => {
+          state.toDoList[id].isFinished = !state.toDoList[id].isFinished;
+        }),
+
+      onTaskStart: () => set((state) => void (state.newTask.isStarted = true)),
+
+      onTitleChange: (event) =>
+        set((state) => {
+          state.newTask.title = event.target.value;
+        }),
+
+      onDescChange: (event) =>
+        set((state) => {
+          state.newTask.desc = event.target.value;
+        }),
+
+      resetNewTask: () =>
+        set((state) => {
+          state.newTask = { title: "", desc: "", isStarted: false };
+        }),
+
+      createNewTask: () => {
+        set((state) => {
+          Object.assign(state.toDoList, {
+            [state.newTask.title]: {
+              title: state.newTask.title,
+              desc: state.newTask.desc,
+              isFinished: false,
+            },
+          });
+        });
+        get().actions.resetNewTask();
+      },
     },
   }))
 );
+
+export const useToDoActions = () => useToDoStore((state) => state.actions);
